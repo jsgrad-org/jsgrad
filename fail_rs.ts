0f42777a139411b0e19f273a1f9bffcf695f16fd
Karel Nagel <nagelkarel@gmail.com>
2025-04-03 12:29:06 +0300

    using ffi rs structs only

--- fail_rs.ts
+++ fail_rs.ts
@@ -1,61 +1,76 @@
-import { arrayConstructor, createPointer, DataType, FFITypeTag, load, open, unwrapPointer } from 'ffi-rs'
+import { createPointer, DataType, load, open } from 'ffi-rs'
 
 const PATH = '/usr/lib/libwebgpu_dawn.so'
 const library = 'dawn'
 open({ path: PATH, library })
 
-const features = new Uint8Array(24)
-features.set([0, 0, 0, 0, 0, 0, 0, 0], 0)
-features.set([1, 0, 0, 0], 8)
-features.set([1, 0, 0, 0, 0, 0, 0, 0], 16)
+const Features = {
+  nextInChain: DataType.U64,
+  timedWaitAnyEnable: DataType.I32,
+  timedWaitAnyMaxCount: DataType.U64,
+  ffiTypeTag: DataType.StackStruct,
+}
+const Desc = {
+  nextInChain: DataType.U64,
+  features: Features,
+  ffiTypeTag: DataType.StackStruct,
+}
+const desc = createPointer({
+  paramsType: [Desc],
+  paramsValue: [{
+    nextInChain: 0,
+    features: {
+      nextInChain: 0,
+      timedWaitAnyEnable: 1,
+      timedWaitAnyMaxCount: 1,
+    },
+  }],
+})[0]
 
-const desc = new Uint8Array(32)
-desc.set([0, 0, 0, 0, 0, 0, 0, 0], 0)
-desc.set(features, 8)
 const instance = load({
   library,
   funcName: 'wgpuCreateInstance',
   retType: DataType.External,
-  paramsType: [DataType.U8Array],
+  paramsType: [DataType.External],
   paramsValue: [desc],
 })
 console.log(instance)
 
-const opts = new Uint8Array(40)
-opts.set([0, 0, 0, 0, 0, 0, 0, 0], 0)
-opts.set([0, 0, 0, 0, 0, 0, 0, 0], 8)
-opts.set([0, 0, 0, 0], 16)
-opts.set([2, 0, 0, 0], 20)
-opts.set([0, 0, 0, 0], 24)
-opts.set([0, 0, 0, 0], 28)
-opts.set([0, 0, 0, 0], 32)
-
-const cb = new Uint8Array(32)
-cb.set([0, 0, 0, 0, 0, 0, 0, 0], 0)
-cb.set([1, 0, 0, 0], 8)
-cb.set([0, 0, 0, 0, 0, 0, 0, 0], 16)
-cb.set([0, 0, 0, 0, 0, 0, 0, 0], 24)
-
-console.log([...desc], desc.length)
-console.log([...opts], opts.length)
-console.log([...cb], cb.length)
-
-const cbType = {
+const Opts = {
+  nextInChain: DataType.U64,
+  compatibleSurface: DataType.U64,
+  featureLevel: DataType.I32,
+  powerPreference: DataType.I32,
+  backendType: DataType.I32,
+  forceFallbackAdapter: DataType.I32,
+  compatibilityMode: DataType.I32,
+  ffiTypeTag: DataType.StackStruct,
+}
+const opts = createPointer({
+  paramsType: [Opts],
+  paramsValue: [{
+    nextInChain: 0,
+    compatibleSurface: 0,
+    featureLevel: 0,
+    powerPreference: 2,
+    backendType: 0,
+    forceFallbackAdapter: 0,
+    compatibilityMode: 0,
+  }],
+})[0]
+const Cb = {
   nextInChain: DataType.U64,
   mode: DataType.U64,
   callback: DataType.U64,
   userdata: DataType.U64,
   ffiTypeTag: DataType.StackStruct,
 }
-const optsPtr = createPointer({
-  paramsType: [DataType.U8Array],
-  paramsValue: [opts],
-})
+console.log(instance, opts)
 const future = load({
   library,
   funcName: 'wgpuInstanceRequestAdapterF',
   retType: DataType.U64,
-  paramsType: [DataType.External, DataType.External, cbType],
-  paramsValue: [instance, unwrapPointer(optsPtr)[0], { nextInChain: 0, mode: 1, callback: 0, userdata: 0 }],
+  paramsType: [DataType.External, DataType.External, Cb],
+  paramsValue: [instance, opts, { nextInChain: 0, mode: 0, callback: 0, userdata: 0 }],
 })
 console.log(future)
