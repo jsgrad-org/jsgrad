1c9247b39ae26998d54351aa10807982906c9d98
Karel Nagel <nagelkarel@gmail.com>
2025-04-08 17:03:50 +0300

    install test

---
+++ test/install.test.js
@@ -0,0 +1,9 @@
+import { env, is_eq, Tensor } from '@jsgrad/jsgrad'
+
+for (const device in env.DEVICES) {
+  if (['DISK', 'CLOUD', 'NULL'].includes(device)) continue
+  await new env.DEVICES[device](device).init()
+
+  const test = await new Tensor([1, 2, 3], { device }).mul(2).tolist()
+  if (!is_eq(test, [2, 4, 6])) throw new Error(`got ${test} instead of [2, 4, 6]`)
+}
