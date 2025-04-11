508cc43089bb6e3c163cd2aa8a7fc2fecd72b442
Karel Nagel <nagelkarel@gmail.com>
2025-04-11 11:19:25 +0300

    working

--- notebook/src/App.tsx
+++ notebook/src/App.tsx
@@ -1,7 +1,9 @@
 import { createContext, type ReactNode, useContext, useRef, useState } from 'react'
 import { Editor, useMonaco } from '@monaco-editor/react'
-import { Uri, Range, KeyMod, KeyCode, editor } from 'monaco-editor'
+import { Uri, Range, KeyMod, KeyCode } from 'monaco-editor'
 import { useEffect } from 'react'
+import { PlayIcon } from 'lucide-react'
+import { Console, Hook, Unhook } from 'console-feed'
 
 const CELL_TYPES = ['code', 'markdown']
 type CellType = 'code' | 'markdown'
@@ -93,13 +95,13 @@ const Cells = () => {
   const { cells } = useNotebook()
   const startEnd = getStartEnd(cells)
   return (
-    <div className="flex flex-col gap-6 bg-[#1e1e1e] text-white p-10">
-      <CodeInit />
+    <div className="flex flex-col gap-6 bg-[#1e1e1e] h-screen text-white p-10">
+      <CodeInit code={cellsToString(cells)} />
       {cells.map((cell, i) => {
         return (
           <div key={i} className="">
             {cell.type === 'markdown' && <MarkdownBlock content={cell.content} />}
-            {cell.type === 'code' && <CodeBlock start={startEnd[i].start} end={startEnd[i].end} />}
+            {cell.type === 'code' && <CodeBlock content={cell.content} start={startEnd[i].start} end={startEnd[i].end} />}
           </div>
         )
       })}
@@ -107,15 +109,17 @@ const Cells = () => {
   )
 }
 
-export const CodeInit = () => {
+export const CodeInit = ({ code }: { code: string }) => {
   const monaco = useMonaco()
-  const { cells, setCells } = useNotebook()
+  const { setCells } = useNotebook()
+
   useEffect(() => {
     if (!monaco) return
+
     const uri = Uri.file('notebook.ts')
     let model = monaco.editor.getModel(uri)
     if (model) return
-    model = monaco.editor.createModel(cellsToString(cells), 'typescript', uri)
+    model = monaco.editor.createModel(code, 'typescript', uri)
     model.onDidChangeContent((e) => {
       const cells = codeToCells(model.getLinesContent())
       setCells(cells)
@@ -126,9 +130,13 @@ export const CodeInit = () => {
       module: monaco.languages.typescript.ModuleKind.ESNext,
       allowNonTsExtensions: true,
       moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+
       noEmit: true,
     })
-
+  }, [monaco])
+  const [imported, setImported] = useState<string[]>([])
+  useEffect(() => {
+    if (!monaco) return
     const loadPackage = async (name: string) => {
       const version = await fetch(`https://data.jsdelivr.com/v1/packages/npm/${name}`)
         .then((x) => x.json())
@@ -150,67 +158,135 @@ export const CodeInit = () => {
       })
       await Promise.all(promises)
     }
-
-    loadPackage('@jsgrad/jsgrad')
-    loadPackage('@jsgrad/models')
-  }, [monaco])
+    const packages = [...code.matchAll(/import\s*{[^}]+}\s*from\s*['"]([^'"]+)['"]/g)].map((match) => match[1])
+    Promise.all(
+      packages
+        .filter((x) => !imported.includes(x))
+        .map(async (x) => {
+          await loadPackage(x)
+          setImported((i) => [...i, x])
+        }),
+    )
+  }, [code, monaco])
 
   return null
 }
 
-export const CodeBlock = ({ start, end }: { start: number; end: number }) => {
+const runJS = async (code: string) => {
+  code = code.trim()
+
+  if (/^\s*\{/.test(code) && /\}\s*$/.test(code)) {
+    code = `(${code})`
+  }
+
+  code = code.replaceAll('const ', 'window.')
+  code = code.replaceAll('let ', 'window.')
+
+  code = code.replace(/import\s*{([^}]+)}\s*from\s*['"]([^'"]+)['"]/g, (_, imports, pkg) => {
+    const cleanedImports = imports.trim().replace(/\s+/g, ' ')
+    return `const {${cleanedImports}} = await import("https://cdn.jsdelivr.net/npm/${pkg}/+esm")\nObject.assign(window,{${cleanedImports}})`
+  })
+
+  try {
+    const lines = code.split('\n').filter((line) => line.trim() !== '')
+    if (lines.length === 0) return
+
+    const lastLine = lines[lines.length - 1].trim()
+    const result = await eval(`
+(async () => {
+  ${code}
+  return ${lastLine};
+})()`)
+
+    if (result !== undefined && result !== null) console.log(result)
+  } catch (e) {
+    console.error(e)
+  }
+}
+export const CodeBlock = ({ start, end, content }: { content: string; start: number; end: number }) => {
   const lineHeight = 18
   const ref = useRef<any>(null)
   const range = useRef<Range>(null)
+  const [logs, setLogs] = useState<any[]>([])
+
   useEffect(() => {
     range.current = new Range(start, 1, end - 1, Number.MAX_SAFE_INTEGER)
     ref.current?.revealRange(range.current, 1)
   }, [start, end])
-  return (
-    <Editor
-      defaultPath={Uri.file('notebook.ts').toString()}
-      height={(end - start) * lineHeight}
-      onMount={(editor) => {
-        ref.current = editor
-        range.current = new Range(start, 1, end - 1, Number.MAX_SAFE_INTEGER)
 
-        editor.revealRange(range.current, 1)
-        editor.onDidScrollChange((e) => {
-          editor.revealRange(range.current!, 1)
-        })
-        editor.addAction({
-          id: 'custom-select-all',
-          label: 'Select Cell Content',
-          keybindings: [KeyMod.CtrlCmd | KeyCode.KeyA],
-          run: () => {
-            editor.setSelection(range.current!)
-          },
-        })
-        editor.onDidChangeCursorPosition((e) => {
-          const pos = e.position
-          const start = range.current!.getStartPosition()
-          const end = range.current!.getEndPosition()
-          if (pos.isBefore(start)) editor.setPosition(start)
-          else if (end.isBefore(pos)) editor.setPosition(end)
-        })
-      }}
-      options={{
-        lineNumbers: (e) => (e - start + 1) as any,
-        stickyScroll: { enabled: false },
-        wordWrap: 'off',
-        minimap: { enabled: false },
-        lineHeight,
-        formatOnType: true,
-        scrollbar: {
-          vertical: 'hidden',
-          horizontal: 'auto',
-          handleMouseWheel: false,
-        },
-        overviewRulerLanes: 0,
-        overviewRulerBorder: false,
-      }}
-      theme="vs-dark"
-    />
+  const run = async () => {
+    setLogs([])
+    const hookedConsole = Hook(window.console, (log) => setLogs((currLogs) => [...currLogs, log]), false)
+
+    await runJS(content)
+
+    Unhook(hookedConsole)
+  }
+  return (
+    <div className="relative flex border border-white/10 rounded-md">
+      <div className="w-8 bg-white/5 flex items-center justify-center hover:bg-white/10 cursor-pointer" onClick={run}>
+        <PlayIcon className="h-4" />
+      </div>
+      <div className="w-full">
+        <Editor
+          defaultPath={Uri.file('notebook.ts').toString()}
+          height={(end - start) * lineHeight}
+          onMount={(editor) => {
+            ref.current = editor
+            range.current = new Range(start, 1, end - 1, Number.MAX_SAFE_INTEGER)
+
+            editor.revealRange(range.current, 1)
+            editor.onDidScrollChange((e) => {
+              editor.revealRange(range.current!, 1)
+            })
+            editor.addAction({
+              id: 'custom-select-all',
+              label: 'Select Cell Content',
+              keybindings: [KeyMod.CtrlCmd | KeyCode.KeyA],
+              run: () => {
+                editor.setSelection(range.current!)
+              },
+            })
+            editor.onDidChangeCursorPosition((e) => {
+              const pos = e.position
+              const start = range.current!.getStartPosition()
+              const end = range.current!.getEndPosition()
+              if (pos.isBefore(start)) editor.setPosition(start)
+              else if (end.isBefore(pos)) editor.setPosition(end)
+            })
+          }}
+          options={{
+            lineNumbers: (e) => (e - start + 1) as any,
+            stickyScroll: { enabled: false },
+            wordWrap: 'off',
+            minimap: { enabled: false },
+            lineHeight,
+            formatOnType: true,
+            scrollbar: {
+              vertical: 'hidden',
+              horizontal: 'auto',
+              handleMouseWheel: false,
+              useShadows: false,
+            },
+            overviewRulerLanes: 0,
+            lineDecorationsWidth: 1,
+            lineNumbersMinChars: 3,
+            overviewRulerBorder: false,
+          }}
+          theme="vs-dark"
+        />
+        <div>
+          <Console
+            logs={logs}
+            variant="dark"
+            styles={{
+              LOG_BACKGROUND: 'transparent',
+              BASE_BACKGROUND_COLOR: 'transparent',
+            }}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
