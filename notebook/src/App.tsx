855907726facee2fed449372ab10935615582189
Karel Nagel <nagelkarel@gmail.com>
2025-04-12 11:29:08 +0300

    buttons

--- notebook/src/App.tsx
+++ notebook/src/App.tsx
@@ -2,22 +2,13 @@ import { createContext, type ReactNode, useContext, useRef, useState } from 'rea
 import { Editor, useMonaco } from '@monaco-editor/react'
 import { Uri, Range, KeyMod, KeyCode } from 'monaco-editor'
 import { useEffect } from 'react'
-import { CodeIcon, PlayIcon, TextIcon, type LucideIcon } from 'lucide-react'
+import { CodeIcon, Loader2Icon, PlayIcon, TextIcon, type LucideIcon } from 'lucide-react'
 import { Console, Hook, Unhook } from 'console-feed'
 import { marked } from 'marked'
 import ts from 'typescript'
 
 const CELL_TYPES = ['code', 'markdown']
-type CellType = 'code' | 'markdown'
-type Cell = {
-  type: CellType
-  content: string
-}
-type Notebook = {
-  cells: Cell[]
-  setCells: React.Dispatch<React.SetStateAction<Cell[]>>
-}
-
+const NOTEBOOK = Uri.file('notebook.ts')
 type PackageFile = { type: 'directory'; name: string; files: PackageFile[] } | { type: 'file'; name: string; hash: string; size: number }
 type PackageInfo = {
   name: string
@@ -25,41 +16,91 @@ type PackageInfo = {
   files: PackageFile[]
 }
 
-const pattern = /\/\*\*\s*\[\]\(cell:(\w+)\)\s*\*\//
+const pattern = /\/\*\*\s*\[\]\((.*?)\)\s*\*\//
 
 const codeToCells = (lines: string[]): Cell[] => {
   const out: Cell[] = []
-  let type: CellType | undefined
+  let cell: Cell | undefined
   let currentLines: string[] = []
   const push = () => {
-    if (type === 'markdown') currentLines = currentLines.join('\n').trim().split('\n').slice(1, -1)
-    out.push({ type: type!, content: currentLines.join('\n') })
+    if (cell!.type === 'markdown') currentLines = currentLines.join('\n').trim().split('\n').slice(1, -1)
+    out.push({ ...cell!, content: currentLines.join('\n') })
   }
 
   for (const line of lines) {
     const match = line.match(pattern)
     if (match) {
-      if (type) push()
+      if (cell) push()
       currentLines = []
-      type = match[1] as CellType
-      if (!CELL_TYPES.includes(type)) throw new Error(`Invalid cell type ${type}`)
+      cell = Object.fromEntries(match[1].split(',').map((x) => x.split(':').map((x) => x.trim()))) as Cell
+      if (!CELL_TYPES.includes(cell.type)) throw new Error(`Invalid cell type ${cell.type}`)
     } else {
       currentLines.push(line)
     }
   }
-  if (type) push()
+  if (cell) push()
   return out
 }
 
+type CellType = 'code' | 'markdown'
+type Cell = {
+  type: CellType
+  runOnLoad?: 'true'
+  content: string
+}
+type Notebook = {
+  cells: Cell[]
+  setCells: React.Dispatch<React.SetStateAction<Cell[]>>
+  queue: number[]
+  setQueue: React.Dispatch<React.SetStateAction<number[]>>
+  isRunning: boolean
+  cellIsRunning: Record<number, boolean>
+  cellLogs: Record<number, any[]>
+}
+
 const NotebookContext = createContext<Notebook | undefined>(undefined)
+
+export const NotebookProvider = ({ code, children }: { children: ReactNode; code: string }) => {
+  const [cells, setCells] = useState(() => codeToCells(code.split('\n')))
+  const [queue, setQueue] = useState<number[]>([])
+  const [isRunning, setIsRunning] = useState(false)
+  const [cellLogs, setCellLogs] = useState<Record<number, any[]>>({})
+  const [cellIsRunning, setCellIsRunning] = useState<Record<number, boolean>>({})
+  useEffect(() => {
+    setQueue([...cells.entries()].filter(([_, x]) => x.runOnLoad).map(([i]) => i))
+  }, [])
+  useEffect(() => {
+    if (isRunning || !queue.length) return
+
+    const next = queue.shift()!
+    run(next)
+  }, [queue, isRunning])
+
+  const run = async (index: number) => {
+    if (isRunning) return
+
+    const cell = cells[index]
+    if (!cell || cell.type !== 'code') return
+
+    setIsRunning(true)
+    setCellIsRunning((x) => ({ ...x, [index]: true }))
+    setCellLogs((x) => ({ ...x, [index]: [] }))
+
+    const hookedConsole = Hook(window.console, (log) => setCellLogs((x) => ({ ...x, [index]: [...(x[index] || []), log] })), false)
+
+    await runJS(cell.content)
+
+    setIsRunning(false)
+    setCellIsRunning((x) => ({ ...x, [index]: false }))
+
+    void Unhook(hookedConsole)
+  }
+  return <NotebookContext.Provider value={{ cells, setCells, queue, setQueue, cellIsRunning, cellLogs, isRunning }}>{children}</NotebookContext.Provider>
+}
 const useNotebook = () => {
   const res = useContext(NotebookContext)
   if (!res) throw new Error(`You can access NotebookContext only in the provider`)
   return res
-}
-export const NotebookProvider = ({ code, children }: { children: ReactNode; code: string }) => {
-  const [cells, setCells] = useState(() => codeToCells(code.split('\n')))
-  return <NotebookContext.Provider value={{ cells, setCells }}>{children}</NotebookContext.Provider>
 }
 
 export const App = ({ code }: { code: string }) => {
@@ -90,7 +131,8 @@ const getStartEnd = (cells: Cell[]) => {
 const cellsToString = (cells: Cell[]) => {
   let chunks: string[] = []
   for (const cell of cells) {
-    let chunk = `/** [](cell:${cell.type}) */\n`
+    const args = [`type:${cell.type}`, cell.runOnLoad ? `runOnLoad:true` : '']
+    let chunk = `/** [](${args.filter(Boolean).join(',')}) */\n`
     if (cell.type === 'markdown') chunk += `/**\n${cell.content}\n*/`
     if (cell.type === 'code') chunk += `${cell.content}`
     chunks.push(chunk)
@@ -99,16 +141,41 @@ const cellsToString = (cells: Cell[]) => {
 }
 
 const Cells = () => {
-  const { cells } = useNotebook()
+  const { cells, setQueue } = useNotebook()
   const startEnd = getStartEnd(cells)
   return (
     <div className="flex flex-col h-full min-h-screen pt-10">
-      <CodeInit code={cellsToString(cells)} type="typescript" />
+      <div className="flex gap-2">
+        <button className="cursor-pointer" onClick={() => setQueue(() => [...cells.entries()].filter(([i, x]) => x.type === 'code').map(([i]) => i))}>
+          Run all
+        </button>
+        <button className="cursor-pointer" onClick={() => navigator.clipboard.writeText(cellsToString(cells))}>
+          Copy notebook
+        </button>
+        <button className="cursor-pointer" onClick={() => {}}>
+          Share
+        </button>
+        <button className="cursor-pointer" onClick={() => {}}>
+          Share with minimized URL
+        </button>
+        <a href="https://notebook.jsgrad.org/new">New</a>
+        <button className="cursor-pointer" onClick={() => {}}>
+          Open
+        </button>
+        <button className="cursor-pointer" onClick={() => {}}>
+          Reload
+        </button>
+        <button className="cursor-pointer" onClick={() => {}}>
+          Raw
+        </button>
+      </div>
+
+      <CodeInit type="typescript" />
       {cells.map((cell, i) => {
         return (
           <div key={i} className="hover:bg-white/2 duration-200 py-2 px-10">
-            {cell.type === 'markdown' && <MarkdownBlock content={cell.content} start={startEnd[i].start} end={startEnd[i].end} />}
-            {cell.type === 'code' && <CodeBlock content={cell.content} start={startEnd[i].start} end={startEnd[i].end} />}
+            {cell.type === 'markdown' && <MarkdownBlock index={i} content={cell.content} start={startEnd[i].start} end={startEnd[i].end} />}
+            {cell.type === 'code' && <CodeBlock index={i} content={cell.content} start={startEnd[i].start} end={startEnd[i].end} />}
           </div>
         )
       })}
@@ -117,21 +184,26 @@ const Cells = () => {
 }
 
 type CodeType = 'typescript' | 'javascript'
-export const CodeInit = ({ code, type }: { code: string; type: CodeType }) => {
+export const CodeInit = ({ type }: { type: CodeType }) => {
   const monaco = useMonaco()
-  const { setCells } = useNotebook()
+  const { cells, setCells } = useNotebook()
+  const code = cellsToString(cells)
 
   useEffect(() => {
     if (!monaco) return
 
-    const uri = Uri.file('notebook.ts')
-    let model = monaco.editor.getModel(uri)
+    let model = monaco.editor.getModel(NOTEBOOK)
     if (model) return
-    model = monaco.editor.createModel(code, type, uri)
+
+    model = monaco.editor.createModel(code, type, NOTEBOOK)
     model.onDidChangeContent((e) => {
       const cells = codeToCells(model.getLinesContent())
       setCells(cells)
     })
+  }, [monaco])
+
+  useEffect(() => {
+    if (!monaco) return
 
     monaco.languages.typescript[`${type}Defaults`].setCompilerOptions({
       target: monaco.languages.typescript.ScriptTarget.Latest,
@@ -223,29 +295,80 @@ const runJS = async (code: string) => {
   }
 }
 
-const Block = ({ children, onClick, Icon }: { Icon: LucideIcon; onClick: () => void; children: ReactNode }) => {
+const Block = ({ index, children, onClick, Icon }: { index: number; Icon: LucideIcon; onClick: () => void; children: ReactNode }) => {
+  const { cells, setCells } = useNotebook()
+  const cell = cells[index]
+  const monaco = useMonaco()
+  const set = (cells: Cell[]) => {
+    setCells(cells)
+    monaco!.editor.getModel(NOTEBOOK)!.setValue(cellsToString(cells))
+  }
   return (
     <div className="flex gap-4 relative min-h-14">
       <div className="absolute bottom-2 right-2 z-10 md:relative md:bottom-0 md:right-0 bg-[#1e1e1e] min-h-10 w-10 border border-white/10 shrink-0 flex items-center justify-center hover:bg-[#343434] cursor-pointer rounded-md" onClick={onClick}>
         <Icon className="h-4" />
       </div>
-      <div className="w-full my-auto">{children}</div>
+
+      <div className="w-full my-auto relative">
+        <div className="flex gap-2 absolute top-0 right-0 z-20">
+          <button className="cursor-pointer" onClick={() => set([...cells.slice(0, index + 1), ...cells.slice(index)])}>
+            duplicate
+          </button>
+          <button className="cursor-pointer" onClick={() => set([...cells.slice(0, index), ...cells.slice(index + 1)])}>
+            remove
+          </button>
+          <button
+            className="cursor-pointer"
+            onClick={() => {
+              if (index === 0) return
+              ;[cells[index - 1], cells[index]] = [cells[index], cells[index - 1]]
+
+              set(cells)
+            }}
+          >
+            up
+          </button>
+          <button
+            className="cursor-pointer"
+            onClick={() => {
+              if (index === cells.length - 1) return
+              ;[cells[index + 1], cells[index]] = [cells[index], cells[index + 1]]
+
+              set(cells)
+            }}
+          >
+            down
+          </button>
+          <button
+            className="cursor-pointer"
+            onClick={() => {
+              set(cells.map((x, i) => (i !== index ? x : { ...x, type: x.type === 'code' ? 'markdown' : 'code' })))
+            }}
+          >
+            type
+          </button>
+          <button
+            className="cursor-pointer"
+            onClick={() => {
+              set(cells.map((x, i) => (i !== index ? x : { ...x, runOnLoad: x.runOnLoad ? undefined : 'true' })))
+            }}
+          >
+            run on start
+          </button>
+        </div>
+        {children}
+      </div>
     </div>
   )
 }
-export const CodeBlock = ({ start, end, content }: { content: string; start: number; end: number }) => {
-  const [logs, setLogs] = useState<any[]>([])
 
-  const run = async () => {
-    setLogs([])
-    const hookedConsole = Hook(window.console, (log) => setLogs((currLogs) => [...currLogs, log]), false)
-
-    await runJS(content)
-
-    Unhook(hookedConsole)
-  }
+export const CodeBlock = ({ start, end, content, index }: { index: number; content: string; start: number; end: number }) => {
+  const { setQueue, cellLogs, cellIsRunning } = useNotebook()
+  const run = () => setQueue((x) => [...x, index])
+  const logs = cellLogs[index]
+  const isRunning = cellIsRunning[index]
   return (
-    <Block onClick={run} Icon={PlayIcon}>
+    <Block index={index} onClick={run} Icon={!isRunning ? PlayIcon : Loader2Icon}>
       <Code content={content} start={start} end={end} run={run} />
       <div>
         <Console
@@ -338,7 +461,7 @@ export const Code = ({ start, end, content, run }: { run?: () => void; content: 
   )
 }
 
-export const MarkdownBlock = ({ content, start, end }: { end: number; start: number; content: string }) => {
+export const MarkdownBlock = ({ index, content, start, end }: { index: number; end: number; start: number; content: string }) => {
   const [md, setMd] = useState('')
   const [editor, setEditor] = useState(false)
   useEffect(() => {
@@ -346,7 +469,7 @@ export const MarkdownBlock = ({ content, start, end }: { end: number; start: num
     effect()
   }, [content])
   return (
-    <Block Icon={!editor ? CodeIcon : TextIcon} onClick={() => setEditor((e) => !e)}>
+    <Block index={index} Icon={!editor ? CodeIcon : TextIcon} onClick={() => setEditor((e) => !e)}>
       {!editor && <div className="prose prose-invert py-1 max-w-[1000px]" dangerouslySetInnerHTML={{ __html: md }}></div>}
       {editor && <Code content={content} start={start} end={end} />}
     </Block>
