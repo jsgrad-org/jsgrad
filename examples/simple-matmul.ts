ba2fb2b9bf00cfb18d397b8a2ab22b030e1ccac8
Karel Nagel <nagelkarel@gmail.com>
2025-04-15 10:26:17 +0300

    simple matmul example

--- examples/simple-matmul.ts
+++ examples/simple-matmul.ts
@@ -1,4 +1,7 @@
+/** [](type:code) */
 import { Tensor, dtypes, range, vars } from '@jsgrad/jsgrad'
+
+vars.set("DEBUG", "2")
 
 let dtype_in = vars.get('HALF') ? dtypes.half : vars.get('BFLOAT16') ? dtypes.bfloat16 : dtypes.float
 let acc_dtype = vars.get('ACC_HALF') ? dtypes.half : vars.get('ACC_BFLOAT16') ? dtypes.bfloat16 : undefined
@@ -10,14 +13,14 @@ const N = vars.get_num('N', 4096)
 const M = vars.get_num('M', N)
 const K = vars.get_num('K', N)
 const CNT = vars.get_num('CNT', 10)
-const ATOL = vars.get_num('ATOL', 1e-4)
-const RTOL = vars.get_num('RTOL', 3e-2)
 
-let a = await  Tensor.rand([M, K],undefined, { dtype: dtype_in }).realize(), b = await Tensor.rand([K, N], undefined,{ dtype: dtype_in }).realize()
+let a = await  Tensor.rand([M, K],undefined, { dtype: dtype_in }).realize()
+let b = await Tensor.rand([K, N], undefined, { dtype: dtype_in }).realize()
 
 for (const i of range(CNT)){
-  if (i > 0 && vars.get_num("RAND", 0) != 0){
-    a = await Tensor.rand([M, K],undefined, { dtype: dtype_in }).realize(), b = await Tensor.rand([K, N], undefined, { dtype: dtype_in }).realize()
+  if (i > 0 && vars.get_num("RAND", 0) !== 0){
+    a = await Tensor.rand([M, K], undefined, { dtype: dtype_in }).realize()
+    b = await Tensor.rand([K, N], undefined, { dtype: dtype_in }).realize()
   }
   await a.matmul(b, undefined, acc_dtype).realize()
 }
